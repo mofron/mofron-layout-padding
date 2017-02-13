bfd0e7c7b3a7846c2af8e57ed0478f2e49ac150c
simpart <taria.ki4m@gmail.com>
2017-02-13 22:57:39 +0900

    simple

--- index.js
+++ index.js
@@ -1,58 +1,81 @@
 /**
  * @file Padding.js
+ * @author simpart
  */
 
-mofron.layout.Padding = class extends mofron.layout.Base {
+/**
+ * @class layout.Padding
+ * @brief padding layout class
+ */
+mofron.layout.Padding = class extends mofron.Layout {
 
-    constructor (tp,v) {
+    constructor (tp,val) {
         try {
             super();
-            this.type = null;
-            this.val  = null;
-            
-            var _tp = (tp === undefined) ? null : tp;
-            var _v  = (v === undefined) ? null : v;
-            
-            if ( (null != _tp) && 
-                 (null != _v) ) {
-                if ( ('string' !== (typeof _tp)) ||
-                     ('number' !== (typeof _v)) ) {
-                    throw new Error('invalid parameter');
-                }
-                if ( ('top'    !== _tp) &&
-                     ('right'  !== _tp) &&
-                     ('bottom' !== _tp) &&
-                     ('left'   !== _tp) ) {
-                    throw new Error('invalid parameter');
-                }
-                
-                this.type = _tp;
-                this.val  = _v;
-            } else if ( (null != _tp) &&
-                        (null == _v) ) {
-                if ('number' !== (typeof _tp)) {
-                    throw new Error('invalid parameter');
-                }
-                this.val = _tp;
-            } else {
-                if ('number' !== (typeof _v)) {
-                    throw new Error('invalid parameter');
-                }
-                this.val = _v;
+            var _tp  = (undefined === tp)  ? null : tp;
+            var _val = (undefined === val) ? null : val;
+
+            if (null === _tp) {
+                throw new Error('invalid paramter');
+            } else if ('number' === (typeof _tp)) {
+                _val = _tp;
+                _tp  = '';
             }
+
+            this.m_type  = null;
+            this.m_value = null;
+
+            this.type(_tp);
+            this.value(_val);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    layoutFunc (idx, tgt) {
+    layoutConts (idx, tgt) {
         try {
             var pd = 'padding';
-            if (null !== this.type) {
-                pd += '-' + this.type;
+            if (null !== this.type()) {
+                pd += '-' + this.type();
             }
-            tgt.getVdom().setStyle(pd, this.val + 'px');
+            tgt.vdom().style(pd, this.value() + 'px');
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    type (tp) {
+        try {
+            if (undefined === tp) {
+                return this.m_type;
+            }
+            if ( ('string' != (typeof tp)) ||
+                 ( (''       != tp) &&
+                   ('top'    != tp) &&
+                   ('right'  != tp) &&
+                   ('bottom' != tp) &&
+                   ('left'   != tp) ) ) {
+                throw new Error('invalid parameter');
+            }
+            this.m_type = tp;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
+    value (val) {
+        try {
+            if (undefined === val) {
+                return this.m_value;
+            }
+
+            if ((null === val) || ('number' !== (typeof val))) {
+                throw new Error('invalid parameter');
+            }
+            this.m_value = val;
         } catch (e) {
             console.error(e.stack);
             throw e;
