7372c0e209db6f65add41a3c544c4ca652fce218
simpart <simpart>
2018-07-27 09:47:47 +0900

    supported prm map

--- index.js
+++ index.js
@@ -9,51 +9,12 @@
  */
 mofron.layout.Padding = class extends mofron.Layout {
 
-    constructor (tp,val,mlt) {
+    constructor (po, p2, p3) {
         try {
             super();
             this.name('Padding');
-            this.prmOpt(tp, val, mlt);
-            this.getParam().check(
-                (t) => {
-                    try {
-                        if ( ('string' != (typeof t)) ||
-                             ( ('top'    != t) &&
-                               ('right'  != t) &&
-                               ('bottom' != t) &&
-                               ('left'   != t) ) ) {
-                            throw new Error('invalid parameter');
-                        }
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                },
-                (v) => {
-                    try {
-                        if ('string' !== (typeof v) && ('number' !== typeof v)) {
-                            throw new Error('invalid parameter');
-                        }
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                },
-                (m) => {
-                    try {
-                        if (undefined === m) {
-                            return false;
-                        }
-                        if ('boolean' !== (typeof m)) {
-                            throw new Error('invalid parameter');
-                        }
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                }
-            );
-            
+            this.prmMap('type', 'value', 'multiple');
+            this.prmOpt(po, p2, p3);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -62,12 +23,67 @@ mofron.layout.Padding = class extends mofron.Layout {
     
     contents (idx, tgt) {
         try {
-            let type   = this.value()[0];
-            let px_val = this.value()[1];
-            let pd     = (null === type) ? 'padding' : 'padding-' + type;
-            let style  = {};
-            style[pd]  = px_val + 'px';
+            let pd = (null === this.type()) ? 'padding' : 'padding-' + this.type();
+            let style = {};
+            if ((true === this.multiple()) && ('number' === typeof this.value())) {
+                style[pd] = this.value() * (idx+1) + 'px';
+            } else {
+                style[pd] = ('string' !== typeof this.value()) ? this.value() + 'px' : this.value();
+            }
             tgt.adom().style(style);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    type (tp) {
+        try {
+            if (undefined === tp) {
+                return (undefined === this.m_type) ? null : this.m_type;
+            }
+            if ( ('string' !== (typeof tp)) ||
+                 ( ('top'    !== tp) &&
+                   ('right'  !== tp) &&
+                   ('bottom' !== tp) &&
+                   ('left'   !== tp) ) ) {
+                throw new Error('invalid parameter');
+            }
+            this.m_type = tp;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    value (prm) {
+        try {
+            if (undefined === prm) {
+                /* getter */
+                return (undefined === this.m_value) ? 0 : this.m_value;
+            }
+            /* setter */
+            if ('string' !== (typeof prm) && ('number' !== typeof prm)) {
+                throw new Error('invalid parameter');
+            }
+            this.m_value = prm;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+
+    multiple (prm) {
+        try {
+            if (undefined === prm) {
+                /* getter */
+                return (undefined === this.m_multi) ? false : this.m_multi;
+            }
+            /* setter */
+            if ('boolean' !== typeof prm) {
+                throw new Error('invalid parameter');
+            }
+            this.m_multi = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
